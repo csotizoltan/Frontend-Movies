d4e84fcb63a4643fc98952d4e3a861bd764fc787
csotizoltan <58356357+csotizoltan@users.noreply.github.com>
2021-10-15 08:51:39 +0200

    Filmlista szűrés kész.
    Filmlista szűrés film műfajára és szereplőire.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("betolt").addEventListener("click", () => {
+    document.getElementById("btn_betolt").addEventListener("click", () => {
         betolt();
+        filmLista.innerHTML = ''; // Megakadályozza, hogy a filmlista többször megjelenjen
     });
 });
 
@@ -23,6 +24,7 @@ function betolt() {
             let movies = document.createElement("div");
             movies.innerHTML = element.title + " (" + element.year + ")";
             document.getElementById("filmLista").appendChild(movies);
+
             movies.addEventListener("click", (e) => {
                 console.log("Kattintva:" + element.title);
 
@@ -31,62 +33,83 @@ function betolt() {
                                         + "<a href='adat.com'>" + element.cast + "</a>" + "<br>" 
                                         + "<a href='adat.com'>" + element.genres + "</a>" + "<br>";
 
-                let button = document.createElement("button");
-                document.getElementById("filmAdatok").appendChild(button);
-                button.innerHTML = "reset";
-                button.addEventListener("click", () => {
+                let btn_reset = document.createElement("button");
+                document.getElementById("filmAdatok").appendChild(btn_reset);
+                btn_reset.innerHTML = "reset";
+                btn_reset.addEventListener("click", () => {
                     console.log("Reset gombra kattintva");
                     document.getElementById("filmAdatok").style.display = "none";
+                    document.getElementById("filmLista").style.display = "none";
+                    betolt();
                 });
 
                 document.getElementsByTagName('a')[0].addEventListener('click', (e) => {
                     e.preventDefault();
+                    let szereplo = element.cast;
+                    console.log("Szereplő: " + szereplo);
+
+                    let arrayOfSzereplo = szereplo.toString().split(',');
+
+                    //filmLista.innerHTML.textContent = ''
+                    filmLista.innerHTML = "";
+                    //document.getElementById("filmLista").style.fontSize  = "1.0em";
+
+                    
+
+                    adat
+                    .filter(e => e.cast.includes(arrayOfSzereplo[0]))
+                    .sort((b, a) => {
+                        return a.year - b.year
+                    })
+
+                    /*
+                    .filter(e => e.cast.includes((e) => {
+                        for (let i = 0; i < arrayOfSzereplo.length; i++) {
+                            arrayOfSzereplo[i];
+                        }
+                    }))
+                    */
+
+                    
+                    .forEach(element => {
+                        let movies = document.createElement("div");
+                        movies.innerHTML = element.title + " (" + element.year + ")";
+                        document.getElementById("filmLista").appendChild(movies);
+                    });
                 });
+
                 document.getElementsByTagName('a')[1].addEventListener('click', (e) => {
+                    let mufaj = element.genres;
+                    console.log("Műfaj: " + mufaj);
+
+                    let arrayOfMufaj = mufaj.toString().split(',');
+
+                    filmLista.innerHTML = "";
+
                     e.preventDefault();
+
+                    adat
+                    .filter(e => e.genres.includes(arrayOfMufaj[0]))
+                    //.filter(e => e.genres.includes(mufaj.toString()))
+                    .sort((b, a) => {
+                        return a.year - b.year
+                    })
+                    .forEach(element => {
+                        let movies = document.createElement("div");
+                        movies.innerHTML = element.title + " (" + element.year + ")";
+                        document.getElementById("filmLista").appendChild(movies);
+                    });
+                    
                 });
                 document.getElementById("filmAdatok").style.display = "block";
 
             });
-            
-
-
-            /*
-
-            https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-            
-            const movie = document.createElement("div"); // create a new div element
-            const newContent = document.createTextNode(element.title + " (" + element.year + ")"); // and give it some content
-            //var br = document.createElement("br"); // create a new breakline element
-            //movie.appendChild(br); // add breakline
-            movie.appendChild(newContent); // add the text node to the newly created div
-            const currentDiv = document.getElementById("div1"); // add the newly created element and its content into the DOM
-            document.body.insertBefore(movie, currentDiv);
-            
-            movie.addEventListener('click', (e) => {
-                console.log("Kattintva:" + element.title);
-                filmAdatok.innerHTML += "<div>" + element.title + "</div>";
-                filmAdatok.innerHTML += "<div>" + element.year + "</div>";
-                filmAdatok.innerHTML += "<div>" + "<a href='adat.com'>" + element.cast + "</a></div>";
-                filmAdatok.innerHTML += "<div>" + element.genres + "</div></br>";
-            });
-            */
-
 
         });
     }).catch(e => {
         document.getElementById("error").innerHTML = e.message;
     }).finally(() => {
         console.log("Betöltés befejezve");
+        document.getElementById("filmLista").style.display = "block";
     })
 };
-
-
-
-// Csak egyszer töltödjön be a lista
-// filmAdatok div méretét dinamikusan változtassa
-// Miért egyoldalas JS alkalmazés?
-// Hogyan lehet a forEarch-től külön function-be tenni a megjelenítési kódokat
-
-// JavaScript HTML DOM - Changing CSS
-// https://www.w3schools.com/js/js_htmldom_css.asp
